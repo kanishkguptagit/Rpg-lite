0bffe27ba0c42d5365b77f97c2884abacc0aa1f7
kanishkguptagit <1905902@kiit.ac.in>
2020-08-12 12:19:24 +0530

    Initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    Camera cam;
+    PlayerMotor motor;
+    public LayerMask MovementMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100, MovementMask))
+            {
+                motor.MoveToPoint(hit.point);
+
+                //stop focussing any object
+            }
+        }
+        
+    }
+}
